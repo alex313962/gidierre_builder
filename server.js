2aa1a746edb9c51a2b87158edd75e6138fbfd755
alex313962 <alex313962@gmail.com>
2021-05-10 17:00:13 +0200

    flavour

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const server = express();
 server.all('/', (req, res)=>{
-    res.send('Your bot is alive!')
+    res.send('gidierre_builder sta ruolando fortissimo!')
 })
 
 function keepAlive(){
